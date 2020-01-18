7e644c6cf0f457a2504a50cfbe33e6d69bf22e0d
worldofchris <chris@worldofchris.com>
2020-01-18 23:37:49 +0000

    Added ssl redirect

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ var fs = require('fs');
 
 revision = fs.readFileSync('revision.txt', 'utf8');
 
+// https://stackoverflow.com/questions/7450940/automatic-https-connection-redirect-with-node-js-express
+console.log(app.get('env'))
+
+if (app.get('env') !== 'development') {
+
+	app.get('*', function(req, res) {  
+	    res.redirect('https://' + req.headers.host + req.url);
+
+	    // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+	    // res.redirect('https://example.com' + req.url);
+	})
+}
+
 app.get('/', function (req, res) {
   res.render('index', { version: revision })
 })

9a30076f0288d1f805f6e42ac646b5d0a05ccc19
worldofchris <chris@worldofchris.com>
2020-02-03 19:40:26 +0000

    keep leaving files behind

--- src/index.js
+++ src/index.js
@@ -1,5 +1,111 @@
 import Game from './game.js';
 import GfxRepo from './gfx_repo.js';
+import { request } from 'graphql-request'
+
+class preloadScene extends Phaser.Scene {
+
+      constructor() {
+        super({key : 'preloadScene'});
+      }
+
+      preload()
+      {
+        const query = `{
+            texture {name src}
+          }`
+        const graphql_endpoint = window.location.href + 'graphql';
+        request(graphql_endpoint, query).then(data => {
+            data.texture.forEach(value => {
+              this.load.image(value.name, value.src); 
+            });
+            this.load.start();
+          });
+
+        this.load.on('progress', this.updateBar);
+        this.load.on('complete', this.complete, {scene:this.scene});
+      }
+      updateBar(percentage) {
+        console.log("P:" + percentage);
+      }
+
+      complete() {
+        console.log("COMPLETE!");
+        this.scene.start('gameScene');
+      }
+}
+
+class gameScene extends Phaser.Scene {
+
+      constructor(bc) {
+        super({key : 'gameScene'});
+        this.bc = bc
+        this.i = 0;
+        this.gameWidth = 8000 // this.sys.canvas.width;
+        this.canvasWidth = 1280
+      }
+
+      create ()
+      {
+          var repo;
+          var cursors;
+          var repeat;
+
+          console.log('in create');
+          const gameHeight = this.sys.canvas.hight;
+          const sky = this.add.image(0, 0, 'sky').setOrigin(0, 0)  // reset the drawing position of the image to the top-left - default is centre
+          sky.displayWidth = this.gameWidth;
+
+          const bld = this.add.image(1800, 100, 'buildings').setOrigin(0, 0)  // reset the drawing position of the image to the top-left - default is centre
+          const bl2 = this.add.image(3800, 100, 'buildings').setOrigin(0, 0)  // reset the drawing position of the image to the top-left - default is centre
+
+          var textures = {'sky': {0: {'name': 'pet-sky', 
+                                      'src':  'assets/petscii-sky.png'}, 
+                                  1: {'name': 'raster-sky',
+                                      'src':  'assets/sky.png'},
+                                  2: {'name': 'sky',
+                                      'src':  'assets/sky-2012.png'}
+                                 }
+                         };
+
+          repo = new GfxRepo(textures);
+          this.bc.repo = repo;
+          this.bc.sky = sky;
+          this.bc.max_engagement = 2;
+          this.cursors = this.input.keyboard.createCursorKeys();
+
+          this.cameras.main.setViewport(0, 0, this.sys.canvas.width, this.sys.canvas.height);
+
+          this.input.on('pointerdown', function (pointer) {
+              this.bc.inc_engagement();
+          }, this);       
+      }
+
+      update ()
+      {
+
+        if (this.cursors.left.isDown)
+        {
+          if (this.i > 0) {
+            this.i-=10;
+            console.log(this.i)
+            this.cameras.main.setScroll(this.i, 0);
+          }
+        }
+        else if (this.cursors.right.isDown)
+        {
+          if (this.i < this.gameWidth - this.canvasWidth ) {
+            this.i+=10;
+            console.log(this.i)
+            this.cameras.main.setScroll(this.i, 0);
+          }
+        }
+      }
+
+      resize ()
+      {
+
+      }
+}
 
 export default function bc() {
 
@@ -14,95 +120,18 @@ export default function bc() {
               width: canvasWidth,
               height: 720
           },
-          canvas: document.getElementById("game"),
-          scene: {
-              preload: preload,
-              create: create,
-              update: update
-          }
+          canvas: document.getElementById("game")
       };
+      var bc = new Game();
 
       var game = new Phaser.Game(config);
-      var repo;
-      var bc = new Game();
-      var cursors;
-      var repeat;
-      var i = 0;
+      // load scenes
+      game.scene.add('preloadScene', new preloadScene());
+      game.scene.add('gameScene', new gameScene(bc));
+      game.scene.start('preloadScene');
 
-      const gameWidth = 8000 // this.sys.canvas.width;
-
-      function preload ()
-      {
-          this.load.image('petscii-sky', 'assets/sky-pet.png');
-          this.load.image('raster-sky', 'assets/sky.png');
-          this.load.image('sky-2012', 'assets/sky-2012.png');
-
-          this.load.image('buildings', 'assets/buildings.png');
-
-      }
-
-      function create ()
-      {
-          const gameHeight = this.sys.canvas.hight;
-          const sky = this.add.image(0, 0, 'petscii-sky').setOrigin(0, 0)  // reset the drawing position of the image to the top-left - default is centre
-          sky.displayWidth = gameWidth;
-
-          const bld = this.add.image(1800, 100, 'buildings').setOrigin(0, 0)  // reset the drawing position of the image to the top-left - default is centre
-          const bl2 = this.add.image(3800, 100, 'buildings').setOrigin(0, 0)  // reset the drawing position of the image to the top-left - default is centre
-
-
-          var textures = {'sky': {0: {'name': 'petscii-sky', 
-                                      'src':  'assets/petscii-sky.png'}, 
-                                  1: {'name': 'raster-sky',
-                                      'src':  'assets/sky.png'},
-                                  2: {'name': 'sky-2012',
-                                      'src':  'assets/sky-2012.png'}
-                                 }
-                         };
-
-          repo = new GfxRepo(textures);
-          bc.repo = repo;
-          bc.sky = sky;
-          bc.max_engagement = 2;
-          cursors = this.input.keyboard.createCursorKeys();
-
-          this.cameras.main.setViewport(0, 0, this.sys.canvas.width, this.sys.canvas.height);
-
-          this.input.on('pointerdown', function (pointer) {
-              bc.inc_engagement();
-          }, this);
-
-      }
-
-      function update ()
-      {
-
-        if (cursors.left.isDown)
-        {
-          if (i > 0) {
-            i-=10;
-            console.log(i)
-            this.cameras.main.setScroll(i, 0);
-          }
-        }
-        else if (cursors.right.isDown)
-        {
-          if (i < gameWidth - canvasWidth ) {
-            i+=10;
-            console.log(i)
-            this.cameras.main.setScroll(i, 0);
-          }
-        }
-
-
-      }
-
-      function resize ()
-      {
-
-      }
-      return {
-        game:game,
-        bc:bc
-      }
+return {
+    game:game,
+    bc:bc
+  }
 }

1ea4826c3f1d1d1dc1f371b3ce2e685d4f4f565c
worldofchris <chris@worldofchris.com>
2020-02-06 09:07:47 +0000

    Added some proper logging

--- index.js
+++ index.js
@@ -5,6 +5,37 @@ var { buildSchema } = require('graphql');
 const GfxRepo = require("./src/gfx_repo.js")
 
 const express = require('express')
+
+const winston = require('winston');
+
+//////////////////////////////////////////////////////////////////////////
+// This is the vanilla logger to stat with 
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+// 
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+////////////////////////////////////////////////////////////////////////////
+
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -18,6 +49,8 @@ if (app.get('env') !== 'development') {
 }
 
 // Construct a schema, using GraphQL schema language
+logger.log('info', "Starting Bézier City Server");
+
 var schema = buildSchema(`
   type Texture {
     name: String
@@ -38,6 +71,8 @@ var textures = {'sky': {0: {'name': 'pet-sky',
                             'src':  'assets/sky-2012.png'}
                        }
                };
+
+logger.log('info', "Creating graphics repo");
 
 var gfx = new GfxRepo(textures);
 
@@ -66,5 +101,5 @@ app.use('/assets', express.static('public/assets'))
 app.use('/js', express.static('public/js'))
 app.set('view engine', 'pug')
 
-app.listen(port, () => console.log(`listening on port ${port}!`))
+app.listen(port, () => logger.log('info', `listening on port ${port}`))
 
